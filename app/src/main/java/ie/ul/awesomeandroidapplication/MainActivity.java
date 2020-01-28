08c39c241fe148b9514b803b03a49b3b3e066b18
Goetz Botterweck <botterweck@gmail.com>
2020-01-28 19:32:39 +0000

    added Toast to MainActivity.onCreate

--- app/src/main/java/ie/ul/awesomeandroidapplication/MainActivity.java
+++ app/src/main/java/ie/ul/awesomeandroidapplication/MainActivity.java
@@ -2,10 +2,12 @@ package ie.ul.awesomeandroidapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
 import android.content.Intent;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +17,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Context context = getApplicationContext();
+        CharSequence text = "MainActivity.onCreate()";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
     }
 
     public void sendMessage(View view){
